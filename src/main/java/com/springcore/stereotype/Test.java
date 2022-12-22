c6114907d3a059a82c845f3b7b218362836eaa31
Abhishek Tiwari <abhishek.tiwari02@motherson.com>
2022-12-22 10:12:17 +0530

    Done

--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -17,7 +17,8 @@ public class Test {
 	  System.out.println(student.hashCode());
 	  System.out.println("Abhishek");
 	  System.out.println("Yogesh");
-	  
+	  System.out.println("Kishan");
+	  System.out.println("Ramesh");
 	  
 	}
 
